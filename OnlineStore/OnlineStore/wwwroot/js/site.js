f21993f57ec7a4672e74a4cedf421c811429ce1d
Gabriel Dinca <33097664+GabrielDinca25@users.noreply.github.com>
2018-10-20 15:32:59 +0300

    Added login and register forms

--- OnlineStore/OnlineStore/wwwroot/js/site.js
+++ OnlineStore/OnlineStore/wwwroot/js/site.js
@@ -7,3 +7,13 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+// Write your JavaScript code.
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
