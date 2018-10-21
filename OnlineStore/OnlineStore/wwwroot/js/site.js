3cb4d19a9cf70ccbc2de76b1c51610ba18588d6e
Gabriel Dinca <33097664+GabrielDinca25@users.noreply.github.com>
2018-10-21 16:40:23 +0300

    Added admin panel and shopping cart

--- OnlineStore/OnlineStore/wwwroot/js/site.js
+++ OnlineStore/OnlineStore/wwwroot/js/site.js
@@ -17,3 +17,13 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+// Write your JavaScript code.
+var modal = document.getElementById('add-product');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
