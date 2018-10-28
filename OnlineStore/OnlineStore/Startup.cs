10e499922cc608db43b9578a1f4bc1d4a9cc9340
Gabriel Dinca <33097664+GabrielDinca25@users.noreply.github.com>
2018-10-28 10:09:50 +0200

    Added user model

--- OnlineStore/OnlineStore/Startup.cs
+++ OnlineStore/OnlineStore/Startup.cs
@@ -22,6 +22,8 @@ namespace OnlineStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDistributedMemoryCache(); // Adds a default in-memory implementation of IDistributedCache
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +40,8 @@ namespace OnlineStore
             }
 
             app.UseStaticFiles();
+
+            app.UseSession();
 
             app.UseMvc(routes =>
             {
